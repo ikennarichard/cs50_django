c8f2dd227979f57c4ac49168b8f848a9ed5af0ea
Ikenna Richard <oguejioforrichard@gmail.com>
2023-01-02 03:47:44 -1100

    Delete file

--- my_app/expenses_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:item>', views.lists, name='lists'),
-    path('<str:name>', views.notes, name='notes')
-]
